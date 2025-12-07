74006eca0b9cd94c6486eb6fcbd2a3215cd22246
Simon Ong <simonongst@gmail.com>
2025-12-07 14:43:25 +0800

    feat: fetch currencies and pass currencyData to EquipmentCalculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from 'react';
 import TopNavBar from './components/NavBars/TopNavBar';
 import UserNavBar from './components/NavBars/UserNavBar';
 import MainTabs from './components/Tabs/MainTabs';
+import { getAllCurrency } from './services/currency';
+import EquipmentCalculator from './pages/EquipmentCalculator';
 
 function App() {
+  const [currencyData, setCurrencyData] = useState([]);
+
+  async function fetchCurrencies() {
+      try {
+      const data = await getAllCurrency();
+      console.log(data);
+      if (data) setCurrencyData(data);
+    } catch (error) {
+      console.log("Error fetching currency data from BE:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+
   return (
     <div className='h-screen w-full bg-gray-50'>
       <TopNavBar />
@@ -11,6 +30,9 @@ function App() {
       </div>
       <div>
         <MainTabs />
+      </div>
+      <div>
+        <EquipmentCalculator currencyData={currencyData}/>
       </div>
     </div>
   );
