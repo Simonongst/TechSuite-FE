fc27a15541662ea288fd994c389980014bc2e3ae
Simon Ong <simonongst@gmail.com>
2025-12-07 14:47:43 +0800

    feat: create EquipmentSummary component with heading and description

---
+++ src/components/Equipment/EquipmentSummary.jsx
@@ -0,0 +1,12 @@
+function EquipmentSummary() {
+  return (
+    <div className='space-y-2 text-gray-700'>
+      <h2 className='font-semibold'>Equipment Summary</h2>
+      <p className='text-sm'>
+        Live calculation results
+      </p>
+    </div>
+  );
+}
+
+export default EquipmentSummary;
