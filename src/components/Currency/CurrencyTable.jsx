91f443eb29c3e317dee8a79c723087e75837a134
Simon Ong <simonongst@gmail.com>
2025-12-08 18:37:47 +0800

    feat: implement CurrencyTable logic with delete handler

--- src/components/Currency/CurrencyTable.jsx
+++ src/components/Currency/CurrencyTable.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-
-function CurrencyTable() {
+import { deleteCurrency } from '../../services/currency';
+function CurrencyTable({
+  selectedRow,
+  setSelectedRow,
+  setOpenDialog,
+  setSelectedCurrency,
+  currencyData,
+  fetchCurrencies,
+}) {
+  if (!currencyData || currencyData.length === 0) {
+    return <div className='p-4 text-slate-500'>No Currencies available.</div>;
+  }
+  async function handleDelete(currency) {
+    if (!currency?._id) {
+      console.error('No currency selected for deletion');
+      return;
+    }
+    try {
+      await deleteCurrency(currency._id);
+      await fetchCurrencies();
+    } catch (err) {
+      console.error('Error deleting currency:', err);
+    }
+  }
   return (
     <div>CurrencyTable</div>
   )
