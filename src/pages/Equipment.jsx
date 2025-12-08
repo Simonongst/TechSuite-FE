2b7413557f0ec16957e0085c3d4b34aafd3fdc59
Simon Ong <simonongst@gmail.com>
2025-12-08 20:41:30 +0800

    feat: setup states, useEffect, filterData and add in EquipmentTable and EquipmentDialog in Equipment page,

--- src/pages/Equipment.jsx
+++ src/pages/Equipment.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { IoIosAddCircle } from 'react-icons/io';
+import { HiOutlineRefresh } from 'react-icons/hi';
+import EquipmentTable from '../components/Equipment/EquipmentTable';
+import EquipmentDialog from '../components/Equipment/EquipmentDialog';
 
-function Equipment() {
+function Equipment({ equipmentData, fetchEquipment }) {
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [selectedEquipment, setSelectedEquipment] = useState(null);
+  const [tableData, setTableData] = useState(equipmentData);
+
+  useEffect(() => {
+    if (!filter) {
+      setTableData(equipmentData);
+    } else {
+      filterData(filter.value);
+    }
+  }, [equipmentData, filter]);
+
+  function filterData(status) {
+    if (status === null) {
+      setTableData(equipmentData);
+    } else {
+      setTableData(
+        equipmentData.filter((equipment) => equipment.isActive === status)
+      );
+    }
+  }
+
   return (
-    <div>Equipment</div>
-  )
+    <>
+      <EquipmentTable /> 
+      <EquipmentDialog />
+    </>
+  );
 }
 
-export default Equipment
+export default Equipment;
