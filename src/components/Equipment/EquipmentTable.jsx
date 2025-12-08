217c882d0431fe9b61fb00202080ce8aba95c02b
Simon Ong <simonongst@gmail.com>
2025-12-08 22:23:30 +0800

    feat: add handleDelete and empty state message

--- src/components/Equipment/EquipmentTable.jsx
+++ src/components/Equipment/EquipmentTable.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { MdOutlineEdit, MdDeleteOutline } from 'react-icons/md';
+import { deleteEquipment } from '../../services/equipment';
 
-function EquipmentTable() {
-  return (
-    <div>EquipmentTable</div>
-  )
+function EquipmentTable({
+  selectedRow,
+  setSelectedRow,
+  setOpenDialog,
+  setSelectedEquipment,
+  equipmentData,
+  fetchEquipment,
+}) {
+  if (!equipmentData || equipmentData.length === 0) {
+    return <div className='p-4 text-slate-500'>No Equipment available.</div>;
+  }
+  async function handleDelete(equipment) {
+    if (!equipment?._id) {
+      console.error('No equipment selected for deletion');
+      return;
+    }
+    try {
+      await deleteEquipment(equipment._id);
+      await fetchEquipment();
+    } catch (err) {
+      console.error('Error deleting equipment:', err);
+    }
+  }
+  return <div>EquipmentTable</div>;
 }
 
-export default EquipmentTable
+export default EquipmentTable;
