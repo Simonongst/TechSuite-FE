668731058ce9fe7fbeb7ecdc51f14e5ca3e8b0a4
Simon Ong <simonongst@gmail.com>
2025-12-08 20:17:16 +0800

    feat: create EquipmentTable and EquipmentDialog placeholder

---
+++ src/components/Equipment/EquipmentDialog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EquipmentDialog() {
+  return (
+    <div>EquipmentDialog</div>
+  )
+}
+
+export default EquipmentDialog
---
+++ src/components/Equipment/EquipmentTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EquipmentTable() {
+  return (
+    <div>EquipmentTable</div>
+  )
+}
+
+export default EquipmentTable
