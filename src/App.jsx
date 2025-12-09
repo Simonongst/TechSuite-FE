ee5cb98404ecec6be8b996e671a691f153c14d9b
Simon Ong <simonongst@gmail.com>
2025-12-09 13:21:19 +0800

    feat: pass currencyData and fetchCurrencies to Equipment

--- src/App.jsx
+++ src/App.jsx
@@ -69,6 +69,8 @@ function App() {
             <Equipment
               equipmentData={equipmentData}
               fetchEquipment={fetchEquipment}
+              currencyData={currencyData}
+              fetchCurrencies={fetchCurrencies}
             />
           }
         />
