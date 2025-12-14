b7cdbb84e56129ab12b703dd0296a6600b218465
Simon Ong <simonongst@gmail.com>
2025-12-14 23:31:40 +0800

    chore: replacing existing techsuite logo to techsuite2 logo

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext'
-import techsuite from '../assets/techsuite.png';
+import techsuite2 from '../assets/techsuite_2.png';
 
 const SignUp = () => {
   const [eid, setEid] = useState('');
@@ -45,7 +45,7 @@ const SignUp = () => {
 
   return (
     <div className="grid-bg min-h-screen flex items-center justify-center bg-slate-800 flex-col">
-      <img src={techsuite} alt="TechSuite Logo" className="w-80 pb-5" />
+      <img src={techsuite2} alt='TechSuite Logo' className='w-100 pb-5' />
       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow">
         <div>
           <h2 className="text-center text-3xl font-bold text-gray-900">
