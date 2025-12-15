b49b3749d665ece31ffaf9936e2be949ea17c810
Simon Ong <simonongst@gmail.com>
2025-12-15 23:11:51 +0800

    feat: add Records page with refresh icon and RecordTable integration

---
+++ src/pages/Records.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { HiOutlineRefresh } from 'react-icons/hi';
+import RecordTable from '../components/Record/RecordTable';
+
+function Records({
+  recordData,
+  fetchRecords,
+}) {
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  return (
+    <div className='px-4 max-w-7xl mx-auto'>
+      <h3 className='p-3 text-xl font-semibold'>Record Table</h3>
+      <div className='mb-2 flex justify-between items-center'>
+        <div className='flex items-center'>
+          <HiOutlineRefresh
+            size={25}
+            onClick={() => {
+              setSelectedRow(null);
+              fetchRecords();
+            }}
+            className='transition-transform duration-200 hover:rotate-45'
+          />
+        </div>
+      </div>
+      <RecordTable
+        selectedRow={selectedRow}
+        setSelectedRow={setSelectedRow}
+        recordData={recordData.records}
+        fetchRecords={fetchRecords}
+      />
+    </div>
+  );
+}
+
+export default Records;
