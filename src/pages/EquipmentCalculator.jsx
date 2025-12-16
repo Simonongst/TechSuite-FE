0b328e078bb862d0dccf4e1330ee0a6ab893b775
Simon Ong <simonongst@gmail.com>
2025-12-17 00:06:47 +0800

    feat: read editRecord from location.state via useLocation

--- src/pages/EquipmentCalculator.jsx
+++ src/pages/EquipmentCalculator.jsx
@@ -1,28 +1,52 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import EquipmentPlanning from '../components/Equipment/EquipmentPlanning';
 import EquipmentSummary from '../components/Equipment/EquipmentSummary';
 import calculateSummary from '../utils/calculateSummary';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function EquipmentCalculator({ currencyData, equipmentData }) {
+  const location = useLocation();
+  const editRecord = location.state?.record;
+
   const [form, setForm] = useState({
-    currency: 'USD',
-    employees: 0,
-    desktop: 0,
-    laptop: 0,
-    monitor: 0,
-    deskPhone: 0,
-    switchPorts: '',
-  });
+      currency: 'USD',
+      employees: 0,
+      desktop: 0,
+      laptop: 0,
+      monitor: 0,
+      deskPhone: 0,
+      switchPorts: '',
+    }
+  );
+
+  useEffect(() => {
+    console.log('editRecord from location.state:', editRecord)
+    if (editRecord) {
+      setForm(editRecord.form);
+    }
+  }, [editRecord]);
 
   const summary = calculateSummary(form, equipmentData, currencyData);
+
+  const navigate = useNavigate();
+  function handleDone() {
+    navigate('/records');
+  }
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 px-4 py-4 max-w-7xl mx-auto'>
       <div className='lg:col-span-2 bg-white rounded-lg shadow-md p-6'>
-        <EquipmentPlanning currencyData={currencyData} form={form} setForm={setForm} summary={summary}/>
+        <EquipmentPlanning
+          currencyData={currencyData}
+          form={form}
+          setForm={setForm}
+          summary={summary}
+          editRecord={editRecord}
+          handleDone={handleDone}
+        />
       </div>
       <div className='lg:col-span-1 bg-white rounded-lg shadow-md p-6'>
-        <EquipmentSummary summary={summary} form={form}/>
+        <EquipmentSummary summary={summary} form={form} />
       </div>
     </div>
   );
