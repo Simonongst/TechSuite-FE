2ba5d06a9a51ee73108bfb7442013b69871f92e4
Simon Ong <simonongst@gmail.com>
2025-12-17 11:09:57 +0800

    feat: wrap inputs in form tags

--- src/components/Equipment/EquipmentPlanning.jsx
+++ src/components/Equipment/EquipmentPlanning.jsx
@@ -53,154 +53,176 @@ function EquipmentPlanning({
       <p className='text-sm'>
         Enter your organization's requirements to calculate equipment needs
       </p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault
+          handleSave();
+        }}
+      >
+        <fieldset className='mb-4'>
+          <label className='text-sm font-medium' htmlFor='currency'>
+            <h3 className='mt-4 font-semibold'>$ Currency:</h3>
+            <select
+              id='currency'
+              name='currency'
+              value={form.currency}
+              onChange={handleChange}
+              className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm'
+              required
+            >
+              {currencyData
+                .slice()
+                .sort((a, b) => a.code.localeCompare(b.code))
+                .map((currency, idx) => (
+                  <option key={idx} value={currency.code}>
+                    {`${currency.code} - ${
+                      currency.label
+                    } (Rate: ${currency.rateToBase.toLocaleString(undefined, {
+                      minimumFractionDigits: currency.rateToBase < 10 ? 4 : 2,
+                      maximumFractionDigits: currency.rateToBase < 10 ? 4 : 2,
+                    })})`}
+                  </option>
+                ))}
+            </select>
+          </label>
+        </fieldset>
+        <fieldset className='mb-4'>
+          <label className='text-sm font-medium' htmlFor='employees'>
+            <h3 className='mt-4 font-semibold'>Number of Employees:</h3>
+            <input
+              id='employees'
+              type='number'
+              name='employees'
+              value={form.employees}
+              onChange={handleChange}
+              className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm'
+              required
+            ></input>
+          </label>
+        </fieldset>
+        <fieldset className='mt-8 mb-4'>
+          <legend className='text-sm font-semibold'>Computer:</legend>
 
-      <fieldset className='mb-4'>
-        <label className='text-sm font-medium' htmlFor='currency'>
-          <h3 className='mt-4 font-semibold'>$ Currency:</h3>
-          <select
-            id='currency'
-            name='currency'
-            value={form.currency}
-            onChange={handleChange}
-            className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm'
-            required
+          <div className='grid grid-cols-2 gap-4'>
+            <label className='text-sm font-medium' htmlFor='desktop'>
+              <h3 className='mt-4'>Desktop:</h3>
+              <input
+                id='desktop'
+                type='number'
+                name='desktop'
+                disabled={
+                  !equipmentData.find(
+                    (equipment) => equipment.type === 'Desktop'
+                  )?.isActive
+                }
+                value={form.desktop}
+                onChange={handleChange}
+                className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
+                required
+              ></input>
+            </label>
+            <label className='text-sm font-medium' htmlFor='laptop'>
+              <h3 className='mt-4'>Laptop:</h3>
+              <input
+                id='laptop'
+                type='number'
+                name='laptop'
+                value={form.laptop}
+                disabled={
+                  !equipmentData.find(
+                    (equipment) => equipment.type === 'Laptop'
+                  )?.isActive
+                }
+                onChange={handleChange}
+                className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
+                required
+              ></input>
+            </label>
+          </div>
+        </fieldset>
+        <fieldset className='mb-4'>
+          <label className='text-sm font-medium' htmlFor='monitor'>
+            <h3 className='mt-4 font-semibold'>Monitor:</h3>
+            <input
+              id='monitor'
+              type='number'
+              name='monitor'
+              disabled={
+                !equipmentData.find((equipment) => equipment.type === 'Monitor')
+                  ?.isActive
+              }
+              value={form.monitor}
+              onChange={handleChange}
+              className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
+              required
+            ></input>
+          </label>
+        </fieldset>
+        <fieldset className='mb-4'>
+          <label className='text-sm font-medium' htmlFor='deskPhone'>
+            <h3 className='mt-4 font-semibold'>Desk Phone:</h3>
+            <input
+              id='deskPhone'
+              type='number'
+              name='deskPhone'
+              disabled={
+                !equipmentData.find(
+                  (equipment) => equipment.type === 'Desk Phone'
+                )?.isActive
+              }
+              value={form.deskPhone}
+              onChange={handleChange}
+              className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
+              required
+            ></input>
+          </label>
+        </fieldset>
+        <fieldset className='mb-4'>
+          <label className='text-sm font-medium' htmlFor='switchPorts'>
+            <h3 className='mt-4 font-semibold'>Network Switch Ports:</h3>
+            <select
+              id='switchPorts'
+              name='switchPorts'
+              value={form.switchPorts}
+              onChange={handleChange}
+              className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm'
+              required
+            >
+              <option disabled value=''>
+                Select Switch Type
+              </option>
+              <option value='24'>24</option>
+              <option value='48'>48</option>
+            </select>
+          </label>
+        </fieldset>
+        <div className='mt-12 mb-4 flex justify-end gap-4'>
+          <button
+            type='submit'
+            className='text-sm px-4 py-2 bg-blue-600 text-white rounded shadow hover:bg-blue-700'
           >
-            {currencyData
-              .slice()
-              .sort((a, b) => a.code.localeCompare(b.code))
-              .map((currency, idx) => (
-                <option key={idx} value={currency.code}>
-                  {`${currency.code} - ${
-                    currency.label
-                  } (Rate: ${currency.rateToBase.toLocaleString(undefined, {
-                    minimumFractionDigits: currency.rateToBase < 10 ? 4 : 2,
-                    maximumFractionDigits: currency.rateToBase < 10 ? 4 : 2,
-                  })})`}
-                </option>
-              ))}
-          </select>
-        </label>
-      </fieldset>
-      <fieldset className='mb-4'>
-        <label className='text-sm font-medium' htmlFor='employees'>
-          <h3 className='mt-4 font-semibold'>Number of Employees:</h3>
-          <input
-            id='employees'
-            type='number'
-            name='employees'
-            value={form.employees}
-            onChange={handleChange}
-            className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm'
-            required
-          ></input>
-        </label>
-      </fieldset>
-      <fieldset className='mt-8 mb-4'>
-        <legend className='text-sm font-semibold'>Computer:</legend>
-
-        <div className='grid grid-cols-2 gap-4'>
-          <label className='text-sm font-medium' htmlFor='desktop'>
-            <h3 className='mt-4'>Desktop:</h3>
-            <input
-              id='desktop'
-              type='number'
-              name='desktop'
-              disabled={!equipmentData.find(equipment => equipment.type === 'Desktop')?.isActive}
-              value={form.desktop}
-              onChange={handleChange}
-              className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
-              required
-            ></input>
-          </label>
-          <label className='text-sm font-medium' htmlFor='laptop'>
-            <h3 className='mt-4'>Laptop:</h3>
-            <input
-              id='laptop'
-              type='number'
-              name='laptop'
-              value={form.laptop}
-              disabled={!equipmentData.find(equipment => equipment.type === 'Laptop')?.isActive}
-              onChange={handleChange}
-              className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
-              required
-            ></input>
-          </label>
+            {editRecord ? 'Update Entry' : 'Save Entry'}
+          </button>
+          {editRecord && (
+            <button
+              type='button'
+              onClick={handleDone}
+              className='text-sm px-4 py-2 bg-blue-600 text-white rounded shadow hover:bg-blue-700'
+            >
+              Cancel
+            </button>
+          )}
+          {!editRecord && (
+            <button
+              type='button'
+              onClick={handleReset}
+              className='text-sm px-4 py-2 bg-blue-600 text-white rounded shadow hover:bg-blue-700'
+            >
+              Reset
+            </button>
+          )}
         </div>
-      </fieldset>
-      <fieldset className='mb-4'>
-        <label className='text-sm font-medium' htmlFor='monitor'>
-          <h3 className='mt-4 font-semibold'>Monitor:</h3>
-          <input
-            id='monitor'
-            type='number'
-            name='monitor'
-            disabled={!equipmentData.find(equipment => equipment.type === 'Monitor')?.isActive}
-            value={form.monitor}
-            onChange={handleChange}
-            className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
-            required
-          ></input>
-        </label>
-      </fieldset>
-      <fieldset className='mb-4'>
-        <label className='text-sm font-medium' htmlFor='deskPhone'>
-          <h3 className='mt-4 font-semibold'>Desk Phone:</h3>
-          <input
-            id='deskPhone'
-            type='number'
-            name='deskPhone'
-            disabled={!equipmentData.find(equipment => equipment.type === 'Desk Phone')?.isActive}
-            value={form.deskPhone}
-            onChange={handleChange}
-            className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm disabled:bg-gray-100'
-            required
-          ></input>
-        </label>
-      </fieldset>
-      <fieldset className='mb-4'>
-        <label className='text-sm font-medium' htmlFor='switchPorts'>
-          <h3 className='mt-4 font-semibold'>Network Switch Ports:</h3>
-          <select
-            id='switchPorts'
-            name='switchPorts'
-            value={form.switchPorts}
-            onChange={handleChange}
-            className='mt-2 py-2 px-2 block w-full border-gray-300 rounded-md shadow-sm'
-            required
-          >
-            <option disabled value=''>Select Switch Type</option>
-            <option value='24'>24</option>
-            <option value='48'>48</option>
-          </select>
-        </label>
-      </fieldset>
-      <div className='mt-12 mb-4 flex justify-end gap-4'>
-        <button
-          type='button'
-          onClick={handleSave}
-          className='text-sm px-4 py-2 bg-blue-600 text-white rounded shadow hover:bg-blue-700'
-        >
-          {editRecord ? 'Update Entry' : 'Save Entry'}
-        </button>
-        {editRecord && (
-          <button
-            type='button'
-            onClick={handleDone}
-            className='text-sm px-4 py-2 bg-blue-600 text-white rounded shadow hover:bg-blue-700'
-          >
-            Cancel
-          </button>
-        )}
-        {!editRecord && (
-          <button
-            type='button'
-            onClick={handleReset}
-            className='text-sm px-4 py-2 bg-blue-600 text-white rounded shadow hover:bg-blue-700'
-          >
-            Reset
-          </button>
-        )}
-      </div>
+      </form>
     </div>
   );
 }
