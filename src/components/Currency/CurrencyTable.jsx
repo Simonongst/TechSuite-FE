71bc353d8944e0c3635fa47dfba2cb86667489d7
Simon Ong <simonongst@gmail.com>
2025-12-19 19:26:28 +0800

    refactor: add empty state to row when no currencyData is available

--- src/components/Currency/CurrencyTable.jsx
+++ src/components/Currency/CurrencyTable.jsx
@@ -10,12 +10,8 @@ function CurrencyTable({
   currencyData,
   fetchCurrencies,
 }) {
-
   const { tokens } = useAuth();
 
-  if (!currencyData || currencyData.length === 0) {
-    return <div className='p-4 text-slate-500'>No Currencies available.</div>;
-  }
   async function handleDelete(currency) {
     if (!currency?._id) {
       console.error('No currency selected for deletion');
@@ -51,65 +47,73 @@ function CurrencyTable({
           </tr>
         </thead>
         <tbody className='divide-y divide-gray-200'>
-          {[...currencyData]
-            .sort((a, b) => {
-              if (a.code === 'USD') return -1;
-              if (b.code === 'USD') return 1;
-              return a.code.localeCompare(b.code);
-            })
-            .map((row, idx) => {
-              const isSelected = selectedRow === idx;
-              return (
-                <tr
-                  key={row.code}
-                  onClick={() => setSelectedRow(idx)}
-                  className={`transition-colors
+          {!currencyData || currencyData.length === 0 ? (
+            <tr>
+              <td colSpan={5} className='p-4 text-center text-slate-500'>
+                No currency available.
+              </td>
+            </tr>
+          ) : (
+            [...currencyData]
+              .sort((a, b) => {
+                if (a.code === 'USD') return -1;
+                if (b.code === 'USD') return 1;
+                return a.code.localeCompare(b.code);
+              })
+              .map((row, idx) => {
+                const isSelected = selectedRow === idx;
+                return (
+                  <tr
+                    key={row.code}
+                    onClick={() => setSelectedRow(idx)}
+                    className={`transition-colors
                   ${isSelected ? 'bg-blue-100' : 'hover:bg-gray-50'}`}
-                >
-                  <td className='px-6 py-4 text-sm text-gray-800'>
-                    {row.code}
-                  </td>
-                  <td className='px-6 py-4 text-sm text-gray-800'>
-                    {row.label}
-                  </td>
-                  <td className='px-6 py-4 text-sm text-gray-800'>
-                    {row.rateToBase?.toLocaleString(undefined, {
-                      minimumFractionDigits: row.rateToBase < 10 ? 4 : 2,
-                      maximumFractionDigits: row.rateToBase < 10 ? 4 : 2,
-                    })}
-                  </td>
-                  <td className='px-6 py-4 text-sm text-gray-800'>
-                    {row.isActive ? <span>Yes</span> : <span>No</span>}
-                  </td>
-                  <td className='px-6 py-4 text-sm text-gray-800'>
-                    <div className='flex gap-1'>
-                      <button
-                        className='bg-blue-400 p-1 rounded-lg text-white cursor-pointer'
-                        onClick={() => {
-                          setSelectedCurrency(row);
-                          setOpenDialog(true);
-                        }}
-                      >
-                        <MdOutlineEdit size={20} />
-                      </button>
-                      <button
-                        className={`p-1 rounded-lg text-white cursor-pointer ${
-                          row.code === 'USD'
-                            ? 'bg-gray-300 cursor-not-allowed'
-                            : 'bg-red-400'
-                        }`}
-                        disabled={row.code === 'USD'}
-                        onClick={() => {
-                          handleDelete(row);
-                        }}
-                      >
-                        <MdDeleteOutline size={20} />
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
+                  >
+                    <td className='px-6 py-4 text-sm text-gray-800'>
+                      {row.code}
+                    </td>
+                    <td className='px-6 py-4 text-sm text-gray-800'>
+                      {row.label}
+                    </td>
+                    <td className='px-6 py-4 text-sm text-gray-800'>
+                      {row.rateToBase?.toLocaleString(undefined, {
+                        minimumFractionDigits: row.rateToBase < 10 ? 4 : 2,
+                        maximumFractionDigits: row.rateToBase < 10 ? 4 : 2,
+                      })}
+                    </td>
+                    <td className='px-6 py-4 text-sm text-gray-800'>
+                      {row.isActive ? <span>Yes</span> : <span>No</span>}
+                    </td>
+                    <td className='px-6 py-4 text-sm text-gray-800'>
+                      <div className='flex gap-1'>
+                        <button
+                          className='bg-blue-400 p-1 rounded-lg text-white cursor-pointer'
+                          onClick={() => {
+                            setSelectedCurrency(row);
+                            setOpenDialog(true);
+                          }}
+                        >
+                          <MdOutlineEdit size={20} />
+                        </button>
+                        <button
+                          className={`p-1 rounded-lg text-white cursor-pointer ${
+                            row.code === 'USD'
+                              ? 'bg-gray-300 cursor-not-allowed'
+                              : 'bg-red-400'
+                          }`}
+                          disabled={row.code === 'USD'}
+                          onClick={() => {
+                            handleDelete(row);
+                          }}
+                        >
+                          <MdDeleteOutline size={20} />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })
+          )}
         </tbody>
       </table>
     </div>
